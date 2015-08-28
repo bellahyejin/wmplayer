1209bbe65cb7c5820571b083a318c70ae4e6c85d
nashinemirin <nashinemirin@naver.com>
2015-08-28 11:36:12 +0900

    a

--- wmplayer.v2/WebContent/js/common.js
+++ wmplayer.v2/WebContent/js/common.js
@@ -4,7 +4,7 @@ function setLink(form_name, url, action, params)
 	if (form_name == null) form = $("<form></form>");
 	else form = $(form_name);
 
-	form.attr("action", "/SpringWeb/" + url).attr("method", "post");
+	form.attr("action", "/wmplyer.v2/" + url).attr("method", "post");
 	if (action != null) form.append($("<input></input>").attr("type", "hidden").attr("name", "action").attr("value", action));
 	if (params != null) for (var key in params) form.append($("<input></input>").attr("type", "hidden").attr("name", key).attr("value", params[key]));
 	form.submit();
