015279c293a96ddd6fe243025dc83f0dd0045d4e
nashinemirin <nashinemirin@naver.com>
2015-08-28 10:16:29 +0900

    a

--- wmplayer.v2/WebContent/view/common/ShareboardForm.jsp
+++ wmplayer.v2/WebContent/view/common/ShareboardForm.jsp
@@ -1,53 +1,52 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<link type="text/css" href="${initParam.root}/css/global.css" rel="stylesheet" />
-<link type="text/css" href="${initParam.root}/css/ShareboardForm.css" rel="stylesheet" />
-<form action="shareinput.do" method="post">
-<div class="sharewrite-form">
-	<div class="sharewrite-header">
-		Share Music Video
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<link type="text/css" href="${ initParam.root }/css/global.css" rel="stylesheet" />
+<link type="text/css" href="${ initParam.root }/css/ShareboardForm.css" rel="stylesheet" />
+<script type="text/javascript" src="${ initParam.root }/js/jquery-2.1.4.js"></script>
+<script type="text/javascript" src="${ initParam.root }/js/common.js"></script>
+<script type="text/javascript">
+	if ("${ fail }" == "fail") alert("노래가 없습니다");
+	else if ("${ fail }" == "error") alert("나중에 다시 시도해주세요");
+</script>
+<form id="writeform" method="post">
+	<div class="sharewrite-form">
+		<div class="sharewrite-header">
+			Share Music Video
+		</div>
+		<div class="sharewrite-contents">
+			<table>
+				<tr>
+					<td id="title">곡 명</td>
+					<td id="con"><input type="text" name="board_title" placeholder="  제목을 입력해주세요" /></td>
+				</tr>
+				<tr>
+					<td id="title">아티스트</td>
+					<td id="con"><input type="text" name="board_artist" placeholder="  아티스트를 입력해주세요" /></td>
+				</tr>
+				<tr>
+					<td id="title">어울리는 날씨</td>
+					<td id="con">
+						<select name="weather_custom" id="weather-customer">
+							<option value="">=선택해주세요=</option>
+							<option value="맑음">맑음</option>
+							<option value="흐림">흐림</option>
+							<option value="비">비</option>
+							<option value="눈">눈</option>
+							<option value="바람">바람</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2" id="title">음악에 대한 설명</td>
+				</tr>
+				<tr>
+					<td colspan="2" id="con"><textarea name="board_desc" cols="50" rows="20" placeholder="  내용을 입력해주세요"></textarea></td>
+				</tr>
+			</table>
+		</div>
+		<div id="wm_btn">
+			<input type="button" value="작 성" id="column" class="styled-button-login" onclick="setLink('#writeform', 'share/insert')" />
+			<input type="button" value="취 소" id="column" class="styled-button-login" onclick="setLink(null, 'share', 'list')" />
+		</div>
 	</div>
-	<div class="sharewrite-contents">
-		<table>
-			<tr>
-				<td id="title">곡 명</td>
-				<td id="con">
-					<input type="text" name="musictitle" placeholder="  제목을 입력해주세요" >
-				</td>
-			</tr>
-			<tr>
-				<td id="title">아티스트</td>
-				<td id="con">
-					<input type="text" name="artist" placeholder="  아티스트를 입력해주세요">
-				</td>
-			</tr>
-			<tr>
-				<td id="title">어울리는 날씨</td>
-				<td id="con">
-					<select id="weather-customer" name="weather">
-						<option>=선택해주세요=</option>
-						<option value="맑음">맑음</option>
-						<option value="흐림">흐림</option>
-						<option value="비">비</option>
-						<option value="눈">눈</option>
-						<option value="바람">바람</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="2" id="title">음악에 대한 설명</td>
-			</tr>
-			<tr>
-				<td colspan="2" id="con">
-					<textarea name="contents" rows="20" cols="50" placeholder="  내용을 입력해주세요"></textarea>
-				</td>
-			</tr>
-			
-		</table>
-	</div>
-	<div id="wm_btn">
-		<input type="submit" class="styled-button-login" id="column" value="작 성" />
-		<input type="button" class="styled-button-login" id="column" value="취 소" onclick="location.href='${initParam.root}/wmplayer/sharelist.do'"/>
-	</div>
-</div>
 </form>
---
+++ wmplayer.v2/src/kr/co/wmplayer/sinmina/controller/ShareBoardController.java
@@ -0,0 +1,232 @@
+package kr.co.wmplayer.sinmina.controller;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import kr.co.wmplayer.sinmina.dao.board.ShareboardDAO;
+import kr.co.wmplayer.sinmina.model.dto.board.BoardUserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import util.ListMap;
+import util.StringOperate;
+import youtube.YoutubeSearch;
+
+@Controller
+@RequestMapping(value = "/share")
+public class ShareBoardController
+{
+	@Autowired ShareboardDAO shareboardDAO;
+
+	StringOperate so = StringOperate.getInstance();
+	String action;
+
+	int max_element = 9, page_div = 4;
+
+	@RequestMapping(value = "/temp", method = RequestMethod.GET)
+	public String temp(Model model, HttpServletRequest request)
+	{
+		return main(model, request);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String main(Model model, HttpServletRequest request)
+	{
+		Object userid = request.getSession().getAttribute("success");
+
+		if (userid != null)
+		{
+			String action = request.getParameter("action") == null ? this.action : request.getParameter("action");
+			if (action == null || action.equals("list")) return shareList(model, request);
+			else if (action.equals("content")) return shareContent(model, request);
+			else if (action.equals("write")) return shareWrite(model, request);
+			else return null;
+		}
+		else return "redirect:/intro";
+	}
+
+	public String shareList(Model model, HttpServletRequest request)
+	{
+		int pres_page = request.getParameter("page") == null ? 1 : Integer.parseInt(request.getParameter("page"));
+		request.setAttribute("pres_page", pres_page);
+		return "sharelist";
+	}
+
+	public String shareContent(Model model, HttpServletRequest request)
+	{
+		return "sharedetail";
+	}
+
+	public String shareWrite(Model model, HttpServletRequest request)
+	{
+		return "sharewrite";
+	}
+
+	@RequestMapping(value = "/insert", method = RequestMethod.POST)
+	public String shareInsert(Model model, HttpServletRequest request, BoardUserDTO bean)
+	{
+		YoutubeSearch youtubeSearch = YoutubeSearch.getInstance();
+
+		System.out.println(request.getParameter("board_title"));
+		String id = (String) request.getSession().getAttribute("success");
+		bean.setBoard_title(so.toUpperCase(bean.getBoard_title(), 0));
+		bean.setBoard_artist(so.toUpperCase(bean.getBoard_artist(), 0));
+		try
+		{
+			String videoID = youtubeSearch.getYoutubeId(bean.getBoard_title(), bean.getBoard_artist());
+			String albumcover = youtubeSearch.getThumnailAddr(bean.getBoard_title(), bean.getBoard_artist());
+
+			System.out.println(bean.getBoard_title() + " " + bean.getBoard_artist());
+			System.out.println(videoID + " " + albumcover);
+			if (videoID == null || albumcover == null)
+			{
+				request.setAttribute("fail", "fail");
+				return shareWrite(model, request);
+			}
+			else
+			{
+				bean.setUserID(id);
+				bean.setVideoID(videoID);
+				bean.setAlbumcover(albumcover);
+				return "redirect:/share";
+			}
+		}
+		catch (IOException | InterruptedException e)
+		{
+			e.printStackTrace();
+			request.setAttribute("fail", "error");
+			return shareWrite(model, request);
+		}
+	}
+
+	@ResponseBody @RequestMapping(value = "/listdata", method = RequestMethod.POST, produces = "text/html; charset=utf-8")
+	public String shareData(@RequestParam(value = "weather_custom") String weather_custom, @RequestParam(value = "page") Integer page)
+	{
+		StringBuffer sb = new StringBuffer();
+		String[] weather = (weather_custom == null ? "all" : weather_custom).split(",");
+		List<String> weather_list = new ArrayList<String>();
+		Map<String, Object> map = new ListMap<String, Object>();
+		map.put("select_column", "*");
+		map.put("group_column", null);
+		map.put("table", "board_user");
+		map.put("column", null);
+		map.put("sign", null);
+		map.put("data", null);
+		int count_element = ((BigDecimal) shareboardDAO.count(map).get("cnt")).intValue();
+
+		int pres_page = page == null ? 1 : page.intValue();
+		int start_idx = (pres_page - 1) * max_element;
+		int max_page = ((int) count_element / max_element) + (count_element % max_element == 0 ? 0 : 1);
+		int begin_page = (pres_page <= 1 + page_div || 1 + page_div * 2 >= max_page ? 1 : (pres_page + page_div >= max_page ? max_page - page_div * 2 : pres_page - page_div));
+		int end_page = (pres_page >= max_page - page_div || 1 >= max_page - page_div * 2 ? max_page : (pres_page <= 1 + page_div ? 1 + page_div * 2 : pres_page + page_div));
+		boolean flag;
+
+		weather : for (int i = 0; i < weather.length; i++)
+		{
+			String temp = weather[i];
+			String kor_weather;
+
+			switch (temp)
+			{
+				case "all" :
+				case "sun" :
+					kor_weather = "맑음";
+					if (weather_list.indexOf(kor_weather) == -1) weather_list.add(kor_weather);
+					if (!temp.equals("all")) break;
+				case "rain" :
+					kor_weather = "비";
+					if (weather_list.indexOf(kor_weather) == -1) weather_list.add(kor_weather);
+					if (!temp.equals("all")) break;
+				case "snow" :
+					kor_weather = "눈";
+					if (weather_list.indexOf(kor_weather) == -1) weather_list.add(kor_weather);
+					if (!temp.equals("all")) break;
+				case "cloud" :
+					kor_weather = "흐림";
+					if (weather_list.indexOf(kor_weather) == -1) weather_list.add(kor_weather);
+					if (!temp.equals("all")) break;
+				case "lowcloudy" :
+					kor_weather = "바람";
+					if (weather_list.indexOf(kor_weather) == -1) weather_list.add(kor_weather);
+			}
+
+			if (temp.equals("all")) break weather;
+		}
+
+		map.put("select_column", "*");
+		map.put("get", "all");
+		map.put("weather_custom", weather_list);
+
+		List<BoardUserDTO> list = shareboardDAO.select(map, start_idx, max_element);
+		sb = sb.append("<div id=\"card\">");
+		for (BoardUserDTO bean : list)
+		{
+			sb = sb.append("<div class=\"cardtotal\">")
+							.append("<li>")
+								.append("<a href=\"#\" onclick=\"setLink(null, 'share', 'content', { 'board_seq' : " + bean.getBoard_seq() + " }\">")
+									.append("<div class=\"cardimg\">")
+										.append("<img id=\"image\" src=\"" + bean.getAlbumcover() + "\" />")
+									.append("</div>")
+									.append("<div class=\"back\">")
+										.append("<table>")
+											.append("<tr>")
+												.append("<td id=\"title\" colspan=\"2\">").append(bean.getBoard_title()).append("</td>")
+											.append("</tr>")
+											.append("<tr>")
+												.append("<td class=\"info_title left up\">Artist</td>")
+												.append("<td id=\"artist\">").append(bean.getBoard_artist()).append("</td>")
+											.append("</tr>")
+											.append("<tr>")
+												.append("<td class=\"info_title left\">Weather</td>")
+												.append("<td id=\"gerne\">").append(bean.getWeather_custom()).append("</td>")
+											.append("</tr>")
+											.append("<tr>")
+												.append("<td class=\"writeinfo left\">").append(bean.getUserID()).append("</td>")
+												.append("<td id=\"writedate\">").append(bean.getRegi_day()).append("</td>")
+											.append("</tr>")
+										.append("</table>")
+									.append("</div>")
+								.append("</a>")
+							.append("</li>")
+						.append("</div>");
+		}
+
+		sb = sb.append("</div>")
+					.append("<div class=\"searchboard\">")
+						.append("<div class=\"search\">")
+							.append("<select class=\"search-select\">")
+								.append("<option value=\"제목\">제목</option>")
+								.append("<option value=\"아티스트\">아티스트</option>")
+								.append("<option value=\"작성자\">작성자</option>")
+							.append("</select>")
+							.append("<input class=\"input-text\" type=\"text\" name=\"search\" placeholder=\"검색어를 입력해주세요\" /><input class=\"input-button search-button\" type=\"button\" value=\"Search\" onclick=\"\" />")
+							.append("<div class=\"write\">")
+								.append("<input type=\"button\" class=\"search-button\" id=\"share\" value=\"Write\" onclick=\"setLink(null, 'share', 'write')\" />")
+							.append("</div>")
+						.append("</div>")
+					.append("</div>")
+					.append("<div class=\"pager-container-share\">")
+						.append("<ul class=\"page-list\">")
+							.append("<div id=\"first\">")
+								.append("<li class=\"").append((flag = pres_page == 1) ? "none_a" : "page\" id=\"first-page\"><a href=\"#\" onclick=\"setLink(null, 'share', 'list', { 'page' : 1})").append("\">start<").append(flag ? "" : "/a><").append("/li>")
+							.append("</div>")
+							.append("<div id=\"middle\">");
+
+			for (int i = begin_page; i <= end_page; i++) sb = sb.append("<li class=\"").append((flag = pres_page == i) ? "none_a" : "page\"><a href=\"#\" onclick=\"setLink(null, 'share', 'list', { 'page' : " + i + " })").append("\">").append(i).append("<").append(flag ? "" : "/a><").append("/li>");
+
+			sb = sb.append("</div>")
+						.append("<div id=\"end\">")
+							.append("<li class=\"").append((flag = pres_page == max_page) ? "none_a" : "page\" id=\"end-page\"><a href=\"#\" onclick=\"setLink(null, 'share', 'list', { 'page' : " + max_page + " })").append("\">end<").append(flag ? "" : "/a><").append("/li>")
+						.append("</div>")
+					.append("</ul>")
+				.append("</div>");
+		return sb.toString();
+	}
+}
