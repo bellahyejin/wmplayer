6f2e272c1f71b6e7c1bd24bb37ece5219d60bc02
nashinemirin <nashinemirin@naver.com>
2015-09-03 15:00:35 +0900

    a

--- wmplayer.v2/src/kr/co/wmplayer/sinmina/controller/UserController.java
+++ wmplayer.v2/src/kr/co/wmplayer/sinmina/controller/UserController.java
@@ -5,10 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import kr.co.wmplayer.sinmina.dao.board.ShareboardDAO;
 import kr.co.wmplayer.sinmina.dao.music.MusicDAO;
 import kr.co.wmplayer.sinmina.dao.user.UserInfoDAO;
@@ -16,9 +14,7 @@ import kr.co.wmplayer.sinmina.model.dto.board.BoardUserDTO;
 import kr.co.wmplayer.sinmina.model.dto.music.LikeMusicDTO;
 import kr.co.wmplayer.sinmina.model.dto.music.MusicInfoDTO;
 import kr.co.wmplayer.sinmina.model.dto.user.UserInfoDTO;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +22,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class UserController {
+public class UserController
+{
 
 	@Autowired
 	private UserInfoDAO dao;
@@ -36,102 +33,113 @@ public class UserController {
 	private MusicDAO musicdao;
 
 	@RequestMapping("/intro")
-	public String intro(){
-		return "common/IntroForm";
+	public String intro(HttpSession session)
+	{
+		return session.getAttribute("success") == null ? "common/IntroForm" : "redirect:main";
 	}
 
 	@RequestMapping("/login")
-	public String login(@RequestParam(value="userID") String userID,
-						@RequestParam(value="passwd") String passwd, HttpSession session){
-
-		if(userID != null && passwd != null)
+	public String login(UserInfoDTO bean, HttpSession session)
+	{
+		if (bean.getUserID() != null && bean.getPasswd() != null)
 		{
-			UserInfoDTO user = new UserInfoDTO(userID, passwd);
+			if (dao.loginproccess(bean) != 0)
+			{
+				String id = bean.getUserID();
 
-			if (dao.loginproccess(user) != 0) {
-				session.setAttribute("success", userID);
-				dao.loginLogInsert(userID);
-				return "common/MainForm";
-			} else {
-				return "redirect:intro";
+				session.setAttribute("success", id);
+				dao.loginLogInsert(id);
+				return "redirect:main";
 			}
 		}
-		else
-			return "redirect:intro";
+		return "redirect:intro";
 	}
 
-	@RequestMapping("/findId" )
-	@ResponseBody
-	public String findid(UserInfoDTO user, HttpServletRequest requset){
+	@RequestMapping(value = "/main")
+	public String main()
+	{
+		return "common/MainForm";
+	}
+
+	@ResponseBody @RequestMapping("/findId")
+	public String findid(UserInfoDTO user, HttpServletRequest requset)
+	{
 		String result = null;
-		if(user.getName() == null || user.getEmail() == null) {
-			return 	"해당 정보를 입력해주세요"  //정보를 입력하지 않았을 경우
+		if (user.getName() == null || user.getEmail() == null)
+		{
+			return "해당 정보를 입력해주세요" // 정보를 입력하지 않았을 경우
 					+ "<input class='input-submit' type='button' id='submit-id' value='아이디 찾기' />";
 		}
-		else{
+		else
+		{
 			String userid = dao.findid(user);
-			if(userid != null){
-				result = "고객님의 아이디는 <span id='userid'>"+userid+"</span> 입니다"
-						+ "<input class='input-submit' type='button' id='submit-back' value='뒤로가기' onclick='moveback()'/>";
+			if (userid != null)
+			{
+				result = "고객님의 아이디는 <span id='userid'>" + userid + "</span> 입니다" + "<input class='input-submit' type='button' id='submit-back' value='뒤로가기' onclick='moveback()'/>";
 				return result;
 			}
-			else //회원 정보가 없을 경우
-				return "등록된 회원정보가 없습니다. 다시입력해주세요"
-						+ "<input class='input-submit' type='button' id='submit-id' value='아이디 찾기' />";
+			else // 회원 정보가 없을 경우
+			return "등록된 회원정보가 없습니다. 다시입력해주세요" + "<input class='input-submit' type='button' id='submit-id' value='아이디 찾기' />";
 		}
 	}
 
-	@RequestMapping("/findPass")
-	@ResponseBody
-	public String findpass(UserInfoDTO user){
+	@ResponseBody @RequestMapping("/findPass")
+	public String findpass(UserInfoDTO user)
+	{
 
 		String result;
-		if(user.getUserID() != null && user.getName() != null && user.getEmail() != null){ //파라미터가 있을경우
-			String userpass = dao.findpass(user); //아이디, 이름, 이메일 검사
-			if(userpass != null){//정보가 맞다면
+		if (user.getUserID() != null && user.getName() != null && user.getEmail() != null)
+		{ // 파라미터가 있을경우
+			String userpass = dao.findpass(user); // 아이디, 이름, 이메일 검사
+			if (userpass != null)
+			{// 정보가 맞다면
 				String responsepass = userpass.substring(0, 3);
 
-				for(int i = 0 ; i < userpass.length() - 3; i++){
-					responsepass += "*"; //전체 비밀번호의 앞에 세자리만 보여준다
+				for (int i = 0; i < userpass.length() - 3; i++)
+				{
+					responsepass += "*"; // 전체 비밀번호의 앞에 세자리만 보여준다
 				}
 
-				result ="고객님의 아이디는 <span id='userid'>"+responsepass+"</span> 입니다"
-						+ "<input class='input-submit' type='button' id='submit-back' value='뒤로가기' onclick='moveback()'/>";	;
+				result = "고객님의 패스워드는 <span id='userid'>" + responsepass + "</span> 입니다" + "<input class='input-submit' type='button' id='submit-back' value='뒤로가기' onclick='moveback()'/>";
+				;
 				return result;
 			}
-			else //해당정보가 없을 경우
-				return "등록된 회원정보가 없습니다. 다시입력해주세요"
-						+ "<input class='input-submit' type='button' id='submit-pass' value='비밀번호찾기' />";
-		}else //정보를 입력하지 않았을 경우
-			return "해당 정보를 입력해주세요"
-					+ "<input class='input-submit' type='button' id='submit-pass' value='비밀번호찾기' />";
+			else // 해당정보가 없을 경우
+			return "등록된 회원정보가 없습니다. 다시입력해주세요" + "<input class='input-submit' type='button' id='submit-pass' value='비밀번호찾기' />";
+		}
+		else // 정보를 입력하지 않았을 경우
+		return "해당 정보를 입력해주세요" + "<input class='input-submit' type='button' id='submit-pass' value='비밀번호찾기' />";
 	}
 
 	@RequestMapping("/logout")
-	public String logout(HttpSession session){
+	public String logout(HttpSession session)
+	{
 
 		session.invalidate();
 		return "redirect:intro";
 	}
 
 	@RequestMapping("/userinfo")
-	public String userinfoform(HttpSession session, Model model){
+	public String userinfoform(HttpSession session, Model model)
+	{
 		String pattern = "#####.##";
-		String userid = (String)session.getAttribute("success");
+		String userid = (String) session.getAttribute("success");
 		UserInfoDTO bean = dao.select(userid);
 		model.addAttribute("user", bean);
 		List<BoardUserDTO> list2 = sharedao.selectMyboard(userid);
 		List<LikeMusicDTO> list = musicdao.selectLikeMusic(userid);
-		
+
 		DecimalFormat format = new DecimalFormat(pattern);
-		
+
 		String avgbpm = format.format(musicdao.avgBpm(userid));
-		
+
 		List<MusicInfoDTO> musiclist = new ArrayList<MusicInfoDTO>();
 
-		for(int i = 0 ; i < list.size() ; i++){
+		for (int i = 0; i < list.size(); i++)
+		{
 			MusicInfoDTO music = musicdao.likemusic(list.get(i).getMusicID());
-			if(music != null){
+			if (music != null)
+			{
 				musiclist.add(music);
 			}
 		}
@@ -143,95 +151,72 @@ public class UserController {
 
 		return "userinfo";
 	}
-	
-	@RequestMapping("/update")
-	@ResponseBody
-	public String userupdate(String passwd, String name, String year, String month,
-							 Date birth, String email,
-							 HttpSession session,Model model){
-		String userid = (String)session.getAttribute("success");
+
+	@ResponseBody @RequestMapping("/update")
+	public String userupdate(String passwd, String name, String year, String month, Date birth, String email, HttpSession session, Model model)
+	{
+		String userid = (String) session.getAttribute("success");
 		String birth_dt = new SimpleDateFormat("yyyy/MM/dd").format(birth);
-		
+
 		UserInfoDTO user = new UserInfoDTO(passwd, name, birth_dt, email);
 		user.setUserID(userid);
-		if(dao.update(user)){
-			return "userinfo";
-		}
-		
-		return "userinfo";       
+		if (dao.update(user)) { return "userinfo"; }
+
+		return "userinfo";
 	}
 
-
 	@RequestMapping("/join")
-	public String userjoin(@RequestParam(value="userID", required=false) String userID,
-						   @RequestParam(value="passwd", required=false ) String passwd,
-						   @RequestParam(value="name", required=false) String name,
-						   @RequestParam(value="year", required=false) String year,
-						   @RequestParam(value="month", required=false) String month,
-						   @RequestParam(value="date", required=false) String date,
-						   @RequestParam(value="gender", required=false) String gender,
-						   @RequestParam(value="email_id", required=false) String email_id,
-						   @RequestParam(value="email_addr", required=false) String addr,
-						   Model model){
+	public String userjoin(@RequestParam(value = "userID", required = false) String userID, @RequestParam(value = "passwd", required = false) String passwd, @RequestParam(value = "name", required = false) String name, @RequestParam(value = "year", required = false) String year, @RequestParam(value = "month", required = false) String month, @RequestParam(value = "date", required = false) String date, @RequestParam(value = "gender", required = false) String gender, @RequestParam(value = "email_id", required = false) String email_id, @RequestParam(value = "email_addr", required = false) String addr, Model model)
+	{
+		if (userID != null && passwd != null && gender != null && email_id != null && year != null && month != null && date != null)
+		{
+			String birth = year + "/" + month + "/" + date;
+			String email = email_id + "@" + addr;
 
-		System.out.println("");
-		if(userID != null && passwd != null && gender != null && email_id != null && year != null && month != null
-				&& date != null){
-
-			String birth = year + "/"+month+"/"+date;
-			String email = email_id+"@"+addr;
-
-			UserInfoDTO user = new UserInfoDTO(userID,passwd,name,birth,gender, email);
-			if(dao.infoInsert(user)){
+			UserInfoDTO user = new UserInfoDTO(userID, passwd, name, birth, gender, email);
+			if (dao.infoInsert(user))
+			{
 				return "redirect:intro";
-			}else{
+			}
+			else
+			{
 				model.addAttribute("alertMsg", "<script>alert('가입에 실패 하였습니다. 다시 시도 해주세요')</script>");
 				return "redirect:intro";
 			}
-
-		}else{
+		}
+		else
+		{
 			model.addAttribute("alertMsg", "<script>alert('회원 정보를 입력해주세요')</script>");
 			return "redirect:intro";
 		}
 	}
 
-	//유효성 검사 중복확인과 pass워드
-	@RequestMapping("/duplicationid")
-	@ResponseBody
-	public String duplicationid(String userID){
-		System.out.println("userID: "+userID);
-		int result =dao.selectcheck(userID);
-		if(result == 1) return "unable";
+	// 유효성 검사 중복확인과 pass워드
+	@ResponseBody @RequestMapping("/duplicationid")
+	public String duplicationid(String userID)
+	{
+		System.out.println("userID: " + userID);
+		int result = dao.selectcheck(userID);
+		if (result == 1) return "unable";
 		else return "able";
 	}
 
-	//drop
+	// drop
 	@RequestMapping("/dropform")
-	public String dropform(){
+	public String dropform()
+	{
 		return "drop";
 	}
 
 	@RequestMapping("/userdrop")
-	public String userdrop(@RequestParam(value="dropreason", required=false,defaultValue="5") int dropreason,
-						   @RequestParam(value="etctext") String etctext,
-						   HttpSession session,
-						   @RequestParam(value="action",required=false) String action,
-						   @RequestParam(value="userid", required=false) String userid){
-
-		String user = null;
-		if(action == null){
-			user = (String) session.getAttribute("success");
-			session.invalidate();
-		}else if(action.equals("manager_drop")){
-			etctext="운영자의 의한 탈퇴";
-			user = userid;
-			return "manager";
-		}
+	public String userdrop(@RequestParam(value = "dropreason", required = false, defaultValue = "5") int dropreason, @RequestParam(value = "etctext") String etctext, HttpSession session)
+	{
+		String user = (String) session.getAttribute("success");
+		session.invalidate();
 		dao.dropupdate(dropreason);
-		if(dropreason == 5) dao.dropReason(etctext);
+		if (dropreason == 5) dao.dropReason(etctext);
 		dao.delete(user);
 
 		return "redirect:intro";
 	}
-
 }
